3545a8acb9fa141929846b339a93d2ea2492a032
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-01-24 14:16:13 -0300

    adicionando models e pacote fluentvalidation no projeto

---
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DevIO.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
