cdf0601dc29ed3f7bcd3f54a30188e30dc70c4cd
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-01-25 16:45:36 -0300

    definindo interface e services no padrao repository

---
+++ src/DevIO.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IFornecedorService<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task<bool> Adicionar(Fornecedor fornecedor);
+        Task<bool> Atualizar(Fornecedor fornecedor);
+        Task<bool> Remover(Fornecedor fornecedor);
+        
+        Task AtualizarEndereco(Endereco endereco);
+    }
+}
