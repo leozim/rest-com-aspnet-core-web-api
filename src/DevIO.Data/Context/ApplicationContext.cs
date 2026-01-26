3b19f8f8a6fa4656b78532c5bea276262d395453
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-01-26 12:06:44 -0300

    criando contexto do db e mapeando as Models pro ORM processar

---
+++ src/DevIO.Data/Context/ApplicationContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
