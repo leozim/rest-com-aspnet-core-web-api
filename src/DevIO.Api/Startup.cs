c0d5bfaeafa9bd8b89aedb9d155f57ad15ee77eb
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-05-30 07:35:42 -0300

    configurando identity

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -68,7 +68,9 @@ namespace DevIO.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            // autenticação precisa sempre vir antes da configuração do MVC
+            app.UseAuthentication();
+            
             app.UseMvcConfiguration();
 
         }
