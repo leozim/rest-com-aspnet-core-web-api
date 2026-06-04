8ae002827fafe32b6ba69c904ea0ad422228786b
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-04 10:24:03 -0300

    criando auth controler e UserDto

---
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[Controller]")]
+    public class AuthController : MainController
+    {
+        public AuthController(INotificador notificador) : base(notificador)
+        {
+        }
+    }
+}
